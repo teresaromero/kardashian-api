5a123de9d8f10a4c88cc159d383dc73ab3513b5c
Teresa <hello@teresaromero.dev>
2021-08-22 16:03:43 +0200

    ✨ feat: create Middleware ValidateCollection

---
+++ middlewares/middlewares.go
@@ -0,0 +1,20 @@
+package middlewares
+
+import (
+	"kardashian_api/database"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func ValidateCollection() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		collection := c.Param("collection")
+		isValid := database.ValidCollection(collection)
+
+		if !isValid {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Collection Not Valid"})
+		}
+		c.Next()
+	}
+}
