d5c6c4c6f7d5a8e7ff9ab7599804fe23b9402095
Teresa <hello@teresaromero.dev>
2021-08-25 17:29:30 +0200

    🪄 refactor: collections

--- controllers/collections.go
+++ controllers/collections.go
@@ -1,69 +1,23 @@
 package controllers
 
 import (
-	"context"
-	"kardashian_api/database"
+	"kardashian_api/custom_errors"
 	"kardashian_api/models"
-
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
+	"kardashian_api/repository"
 )
 
-func ParseCollection(cursor *mongo.Cursor, tableName string, ctx context.Context) (interface{}, error) {
-	var imbd_episode_items []models.IMBDEpisode
-	var imbd_episode_credit_items []models.IMBDEpisodeCredit
-
-	var wiki_episode_items []models.WikiEpisode
-	var err error
-	var result interface{}
-
+func Collection(tableName string) (interface{}, *custom_errors.HttpError) {
 	if tableName == string(models.WikiEpisodes) {
-		err = cursor.All(ctx, &wiki_episode_items)
-		result = wiki_episode_items
+		return repository.GetAllWikiEpisodes()
 	} else if tableName == string(models.IMBDEpisodes) {
-		err = cursor.All(ctx, &imbd_episode_items)
-		result = imbd_episode_items
+		return repository.GetAllIMBDEpisodes()
 	} else if tableName == string(models.IMBDEpisodeCredits) {
-		err = cursor.All(ctx, &imbd_episode_credit_items)
-		result = imbd_episode_credit_items
+		return repository.GetAllIMBDEpisodeCredits()
 	}
-	return result, err
-}
-
-func Collection(tableName string) (items interface{}, err error) {
-	collection := database.Use(tableName)
-
-	ctx, cancel := database.Context()
-	defer cancel()
-
-	cursor, err_find := collection.Find(ctx, bson.D{})
-	if err_find != nil {
-		return nil, err_find
-	}
-	defer cursor.Close(ctx)
-
-	result, err := ParseCollection(cursor, tableName, ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	return result, nil
+	return nil, custom_errors.InvalidCollection(tableName)
 
 }
 
-func AvailableCollections(baseURL string) ([]*models.AvailableCollection, error) {
-
-	cls, err := database.Collections()
-	if err != nil {
-		return nil, err
-	}
-
-	var rsp []*models.AvailableCollection
-	baseUrl := baseURL
-	for _, col := range cls {
-		rsp = append(rsp, &models.AvailableCollection{Name: col, Url: baseUrl + col})
-	}
-
-	return rsp, nil
-
+func AvailableCollections(baseURL string) ([]*models.AvailableCollection, *custom_errors.HttpError) {
+	return repository.GetAvailableCollections(baseURL)
 }
