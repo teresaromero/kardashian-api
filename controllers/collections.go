838abfd6b4640ad436655d4b0d633c68b258ec4b
Tere <romero.teresa@protonmail.com>
2021-08-25 18:34:44 +0200

    Revert "🪄 refactor: change controller model"
    This reverts commit 651a779d76ba93cfab683263e2f3df7c048b5189.

--- controllers/collections.go
+++ controllers/collections.go
@@ -12,21 +12,20 @@ import (
 func Collection(c *gin.Context) {
 	collection := database.Use(c.Param("collection"))
 
-	ctx, cancel := database.Context()
-	defer cancel()
+		ctx, cancel := database.Context()
+		defer cancel()
 
 	cursor, err_find := collection.Find(ctx, bson.D{})
 	if err_find != nil {
 		panic(err_find)
-	}
-	defer cursor.Close(ctx)
+		}
+		defer cursor.Close(ctx)
 
-	// TODO: parse each collection model, using Episode as placeholder for now
-	var coll_items []models.IMBDEpisode
+		// TODO: parse each collection model, using Episode as placeholder for now
 	err_cursor := cursor.All(ctx, &coll_items)
 	if err_cursor != nil {
 		panic((err_cursor))
-	}
+		}
 	c.JSON(http.StatusOK, gin.H{"response": coll_items})
 
 }
