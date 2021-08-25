2cb1c22c71bdb2a432d820ed462c171f23fc1833
Tere <romero.teresa@protonmail.com>
2021-08-25 18:34:44 +0200

    Revert "✨ feat: Add Database connection"
    This reverts commit ff21a2226b3e34cf71e9f70e9f4b8e1f9a8bd8af.

--- app.go
+++ app.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"kardashian_api/config"
-	"kardashian_api/database"
 
 	"github.com/gin-gonic/gin"
 )
 
 func init() {
 	config.LoadEnv()
-	database.Connect()
-
 }
 
 func main() {
