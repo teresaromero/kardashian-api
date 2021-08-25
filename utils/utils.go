086ed5276fe0ffafe793974004811c28b4eafca0
Tere <romero.teresa@protonmail.com>
2021-08-25 18:34:44 +0200

    Revert "✨ feat: new Error response util"
    This reverts commit 23d8d2d9937808888261760514dff4b7d3096321.

--- utils/utils.go
+++
@@ -1,7 +0,0 @@
-package utils
-
-import "github.com/gin-gonic/gin"
-
-func HandleError(c *gin.Context, status int, err error) {
-	c.AbortWithStatusJSON(status, gin.H{"status": status, "message": err.Error()})
-}
